f43cdac50830972922fea743649dc1cef0b15a82
1gyeol-KIM <gksrufsla00@gmail.com>
2021-01-22 19:32:02 +0900

    Update my code

--- morsecode.py
+++ morsecode.py
@@ -245,8 +245,9 @@ def decoding_sentence(morse_sentence):
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
     result = ''
     morse_sentence = morse_sentence.split(' ')
+    print("morse_sentence=", morse_sentence)
     for code in morse_sentence:
-        if code == ' ':
+        if code == '':
             result += ' '
         else:
             result += decoding_character(code)
